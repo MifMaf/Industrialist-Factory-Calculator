4723599430c3a3dde05077460985109cf79593ff
Albin Restorp <albin@restorp.se>
2025-02-16 22:34:07 +0100

    stuff

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+const div1 = document.createElement("div");
+div1.classList.add("redbox", "spin");
+div1.style.left = "calc(50% - 50px)";
+div1.style.top = "calc(50% - 50px)";
+document.body.appendChild(div1);
