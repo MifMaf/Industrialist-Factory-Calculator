f5a19af23e8c6f872774ebad62cd7011f72110e6
Albin Restorp <albin@restorp.se>
2025-02-16 22:43:56 +0100

    yep

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
 const div1 = document.createElement("div");
-div1.classList.add("redbox", "spin");
-div1.style.left = "calc(50% - 50px)";
-div1.style.top = "calc(50% - 50px)";
+div1.id = "redbox";
+div1.classList.add("spin");
 document.body.appendChild(div1);
